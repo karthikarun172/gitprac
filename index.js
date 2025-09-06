fcc6fcd280027c703e17d07661cf05cb942dba6c
karthikarun172 <57855758+karthikarun172@users.noreply.github.com>
2025-09-06 10:31:21 +0530

    ice cream update

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const cors = require("cors");
 console.log("new update");
 app.use(cors());
 
+if (true) {
+  console.log("ice cream");
+} else {
+  console.log("no ice cream");
+}
+
 app.listen(3000, () => console.log("server runing in port 3000"));
