1a4b972daa87a7a5545af616722a40f105672a6c
karthikarun172 <57855758+karthikarun172@users.noreply.github.com>
2025-09-06 10:26:02 +0530

    new update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+console.log("new update");
 app.use(cors());
 
 app.listen(3000, () => console.log("server runing in port 3000"));
